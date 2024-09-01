82e235ba599f6fbe069b1b6c9ccdf7efc6163a5f
trist007 <trist007@darkterminal.net>
2024-09-01 10:32:17 -0500

    good working order

---
+++ cmake_modules/AddGitSubmodule.cmake
@@ -0,0 +1,13 @@
+function(add_git_submodule dir)
+	find_package(Git REQUIRED)
+
+	if (NOT EXISTS ${dir}/CMakeLists.txt)
+		execute_process(COMMAND ${GIT_EXECUTABLE}
+			submodule update --init --recursive -- ${dir}
+				WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+	endif()
+
+	if (EXISTS ${dir}/CMakeLists.txt)
+		add_subdirectory(${dir})
+	endif()
+endfunction(add_git_submodule)
